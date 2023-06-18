9cf3a66d2c4c40f9b9b14e8428aa0a5873b70584
Brettney Chau-Dang <bchaudang@gmail.com>
2023-06-18 17:51:05 -0400

    update tables

--- Employee_Tables.sql
+++ Employee_Tables.sql
@@ -10,7 +10,7 @@ SELECT * FROM departments;
 
 -- dept_emp
 CREATE TABLE dept_emp (
-	emp_no INTEGER PRIMARY KEY,
+	emp_no VARCHAR(30) PRIMARY KEY,
 	dept_no VARCHAR(30) NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
@@ -20,8 +20,8 @@ SELECT * FROM dept_emp;
 
 -- dept_manager
 CREATE TABLE dept_manager (
-	dept_no INTEGER PRIMARY KEY,
-	emp_no INTEGER NOT NULL,
+	dept_no VARCHAR(30) PRIMARY KEY,
+	emp_no VARCHAR(30) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
@@ -29,7 +29,7 @@ SELECT * FROM dept_manager;
 
 -- employees
 CREATE TABLE employees (
-	emp_no INTEGER PRIMARY KEY,
+	emp_no VARCHAR(30) PRIMARY KEY,
 	emp_title_id VARCHAR(30) NOT NULL,
 	birth_date DATE,
 	first_name VARCHAR(30) NOT NULL,
@@ -43,7 +43,7 @@ SELECT * FROM employees;
 
 -- salaries
 CREATE TABLE salaries (
-	emp_no INTEGER PRIMARY KEY,
+	emp_no VARCHAR(30) PRIMARY KEY,
 	salary INTEGER,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
@@ -53,7 +53,7 @@ SELECT * FROM salaries;
 -- titles
 CREATE TABLE titles (
 	title_id VARCHAR(30) PRIMARY KEY,
-	title VARCHAR(30) NOT NULL,
+	title VARCHAR(30) NOT NULL
 );
 
 SELECT * FROM titles;
