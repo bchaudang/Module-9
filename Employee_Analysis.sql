54c046a4a2984e26be87209fcf314b4029f9d799
Brettney Chau-Dang <bchaudang@gmail.com>
2023-06-18 17:52:47 -0400

    commit analysis

--- Employee_Analysis.sql
+++ Employee_Analysis.sql
@@ -7,13 +7,14 @@ SELECT e.emp_no,
 	s.salary
 FROM employees as e
 JOIN salaries as s 
-	ON e.emp_no = s.emp_no;
+	ON e.emp_no = s.emp_no
+ORDER BY emp_no;
 
 -- List the first name, last name, and hire date for the employees who were hired in 1986:
-SELECT first_name, last_name, hire_date
+SELECT emp_no, first_name, last_name, hire_date
 FROM employees
 WHERE hire_date >= '01/01/1986' AND hire_date < '01/01/1987'
-ORDER BY hire_date;
+ORDER BY emp_no;
 
 -- List the manager of each department along with their department number, department name, employee number, last name, and first name:
 SELECT d.dept_no, 
@@ -41,10 +42,10 @@ INNER JOIN departments as d
 ORDER BY emp_no;
 
 -- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B:
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 FROM employees
 WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
-ORDER BY first_name;
+ORDER BY emp_no;
 
 -- List each employee in the Sales department, including their employee number, last name, and first name:
 SELECT e.emp_no, 
@@ -70,16 +71,14 @@ INNER JOIN dept_emp
 INNER JOIN departments as d
 	ON dept_emp.dept_no = d.dept_no
 WHERE dept_name = 'Sales' OR dept_name = 'Development'
-ORDER BY dept_name;
-
-
-
+ORDER BY emp_no;
 
 -- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name):
-
-
-
-
+SELECT last_name, 
+	COUNT (last_name) as frequency_counts
+FROM employees
+GROUP BY last_name
+ORDER BY frequency_counts DESC;
 
 
 
