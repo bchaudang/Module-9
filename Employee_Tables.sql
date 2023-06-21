c1cfe630b7a7ae20f78d3e909fcac17b579ec0c6
Brettney Chau-Dang <130397259+bchaudang@users.noreply.github.com>
2023-06-20 21:32:25 -0400

    Add files via upload
    resubmit

---
+++ Employee_Tables.sql
@@ -0,0 +1,59 @@
+-- Create tables:
+-- titles
+CREATE TABLE titles (
+	title_id VARCHAR(30) PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
+);
+
+SELECT * FROM titles;
+
+-- employees
+CREATE TABLE employees (
+	emp_no VARCHAR(30) PRIMARY KEY,
+	emp_title_id VARCHAR(30) NOT NULL,
+	birth_date DATE,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(30) NOT NULL,
+	hire_date DATE,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+SELECT * FROM employees;
+
+-- departments
+CREATE TABLE departments (
+	dept_no VARCHAR(30) PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL,
+);
+
+SELECT * FROM departments;
+
+-- dept_manager
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(30) PRIMARY KEY,
+	emp_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+SELECT * FROM dept_manager;
+
+-- dept_emp
+CREATE TABLE dept_emp (
+	emp_no VARCHAR(30) PRIMARY KEY,
+	dept_no VARCHAR(30) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM dept_emp;
+
+-- salaries
+CREATE TABLE salaries (
+	emp_no VARCHAR(30) PRIMARY KEY,
+	salary INTEGER,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+SELECT * FROM salaries;
